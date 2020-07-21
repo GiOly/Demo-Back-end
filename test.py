90cc9ed93d8817e42eb7a989f7c2ff5ec5c99d57
GiOly <ly32203918@outlook.com>
2020-07-21 12:12:27 +0800

    修改

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify, render_template, send_from_directory, make_response
 import flask, os
-app = Flask(__name__)
 from model import mfcc
 
-STATE = False
+app = Flask(__name__)
+
 UPLOAD_FOLDER = 'upload'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -29,7 +29,7 @@ def api_download(dirpath, text_filename):
     response = make_response(send_from_directory(dirpath, text_filename, as_attachment=True))
     return response
 
-@app.route("/api/upload", methods=["GET", "POST"],strict_slashes=False)
+@app.route("/api/upload", methods=["GET", "POST"], strict_slashes=False)
 def api_upload():
     file_dir = os.path.join(basedir, app.config['UPLOAD_FOLDER'])
     if not os.path.exists(file_dir):
@@ -40,10 +40,10 @@ def api_upload():
         if audio and allowed_file(audio.filename):
             print("1")
             audio_filename = audio.filename
+            audio_filename = str(audio_filename)
             result = mfcc(audio)
             result = str(result)
-            new_filename = str(audio_filename)
-            audio.save(os.path.join(file_dir, new_filename))
+            audio.save(os.path.join(file_dir, audio_filename))
             data = {"state":"SUCCESS", "result":result}
             return render_template('upload.html', state = "SUCCESS", result = result)
 
