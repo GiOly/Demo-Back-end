a2ab78a8b11c15aec4a5cd0332021be49befbc63
GiOly <ly32203918@outlook.com>
2020-07-25 11:41:38 +0800

    增加显示波形图功能

--- model.py
+++ model.py
@@ -3,18 +3,21 @@ import librosa
 import librosa.display
 import matplotlib.pyplot as plt
 
-def mfcc(audio):
+def tempo(audio):
     y, sr = librosa.load(audio)
-    mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40)
-    return str(mfccs)
+    tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+    return str(tempo)
 
 def waveshow(audio):
     y, sr = librosa.load(audio)
     plt.figure(figsize=(14, 5))
     librosa.display.waveplot(y, sr=sr)
-    filename_str = str(audio.filename)
-    plot_file_path = "static/images/test.jpg"
+    filename_str = os.path.basename(audio)
+    filename_str = filename_str.replace("wav", "jpg")
+    plot_file_path = "static/images/" + filename_str
+    print(plot_file_path)
     plt.savefig(plot_file_path)
+    return plot_file_path
 
 
 
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, render_template, send_from_directory, make_response
 import flask, os
-from model import mfcc, waveshow
+from model import tempo, waveshow
 
 app = Flask(__name__)
 
@@ -29,6 +29,14 @@ def api_download(dirpath, text_filename):
     response = make_response(send_from_directory(dirpath, text_filename, as_attachment=True))
     return response
 
+def return_img_stream(img_path):
+    import base64
+    img_stream = ""
+    with open(img_path, "rb") as img_file:
+        img_stream = img_file.read()
+        img_stream = base64.b64encode(img_stream).decode()
+    return img_stream
+
 @app.route("/api/upload", methods=["GET", "POST"], strict_slashes=False)
 def api_upload():
     file_dir = os.path.join(basedir, app.config['UPLOAD_FOLDER'])
@@ -40,9 +48,12 @@ def api_upload():
         if audio and allowed_file(audio.filename):
             print("已读取文件")
             audio_filename = str(audio.filename)
-            waveshow(audio)
             audio.save(os.path.join(file_dir, audio_filename))
-            return render_template('upload.html')
+            audio_path = os.path.join(file_dir, audio_filename)
+            img_path = waveshow(audio_path)
+            img_stream = return_img_stream(os.path.join("/root/myproject/",img_path))
+            result =  tempo(audio_path)
+            return render_template('upload.html', img_stream = img_stream, state = "SUCCESS",result = result)
 
 if __name__ == "__main__":
     app.run()
